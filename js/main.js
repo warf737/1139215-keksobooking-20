1d26c67c08434f2360a08396716940aff9eb5bc6
dvasilchuk <dvasilchuk@phoenixit.ru>
2020-06-12 17:38:39 +0300

    Добавит обработку событий для отображения карточек объявлений

--- js/main.js
+++ js/main.js
@@ -5,8 +5,14 @@
 var map = document.querySelector('.map');
 var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var mapPins = document.querySelector('.map__pins');
-
 var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+var mapPinMain = document.querySelector('.map__pin--main');
+var form = document.querySelector('.ad-form--disabled');
+var fieldsets = form.querySelectorAll('fieldset');
+var fragment = document.createDocumentFragment();
+var lastActiveElement;
+var popup;
+var popupClose;
 
 var TITLES = [
   {
@@ -126,19 +132,16 @@ var getRandomNumber = function (min, max) {
   var int = min + Math.random() * (max + 1 - min);
   return Math.floor(int);
 };
-
 // получает случайный элемента массива
 var getRandomArrElement = function (arr) {
   return arr[getRandomNumber(0, arr.length - 1)];
 };
-
 // формирует строку вида img/avatars/user08.png
 var getUserAvatar = function (number) {
   var path = 'img/avatars/user0';
   var format = '.png';
   return path + number + format;
 };
-
 // перемешивает значения массива в случайном порядке
 var shuffleArr = function (arr) {
   for (var i = arr.length - 1; i > 0; i--) {
@@ -147,7 +150,6 @@ var shuffleArr = function (arr) {
   }
   return arr;
 };
-
 // преобразует входной массив в массив случайной длины и случайными элементами
 var generateRandomArray = function (arr) {
   var copiedArr = arr;
@@ -156,7 +158,6 @@ var generateRandomArray = function (arr) {
   var features = shuffleArr(copiedArr).slice(0, randomArrLength);
   return features;
 };
-
 // получаем количество гостей в зависимости от количества комнат
 var getNumberRooms = function (rooms) {
   var numberOfRoom = 'не для гостей';
@@ -169,7 +170,6 @@ var getNumberRooms = function (rooms) {
   }
   return numberOfRoom;
 };
-
 // создает одно объявление
 var createAd = function (number) {
   var x = getRandomNumber(COORDS_X.MIN, COORDS_X.MAX);
@@ -207,12 +207,12 @@ var createAd = function (number) {
     }
   };
 };
-
-var renderPin = function (ad) {
+var renderPin = function (ad, number) {
   var pinElement = mapPinTemplate.cloneNode(true);
   pinElement.querySelector('img').src = ad.author.avatar;
   pinElement.style.left = ad.location.x - PIN_SIZE.x / 2 + 'px';
   pinElement.style.top = ad.location.y + PIN_SIZE.y + 'px';
+  pinElement.number = number;
   return pinElement;
 };
 var articleRooms = function (numberRooms) {
@@ -230,7 +230,6 @@ var renderCard = function (ad) {
   var featureList = mapElement.querySelector('.popup__features');
   var photos = mapElement.querySelector('.popup__photos');
   var articleRoom = articleRooms(ad.offer.rooms);
-
   mapElement.querySelector('.popup__title').textContent = ad.offer.title;
   mapElement.querySelector('.popup__text--address').textContent = ad.offer.address;
   mapElement.querySelector('.popup__text--price').textContent = ad.offer.price + '₽/ночь';
@@ -253,7 +252,6 @@ var renderCard = function (ad) {
   }
   return mapElement;
 };
-
 var createAds = function () {
   var adArray = [];
   for (var i = 0; i < AD_COUNT; i++) {
@@ -262,7 +260,76 @@ var createAds = function () {
   }
   return adArray;
 };
+var activatePage = function () {
+  for (var j = 0; j < ads.length; j++) {
+    fragment.appendChild(renderPin(ads[j], j));
+  }
+  // добавляет созданные пины в DOM
+  mapPins.appendChild(fragment);
+  map.classList.remove('.map--faded');
+  form.classList.remove('.ad-form--disabled');
 
+  // делает все поля формы доступными
+  changeAccessibility(fieldsets);
+
+  /* назначает обработчик showPopups на элемент 'Карта',
+     в котором расположены элементы 'Метка объявления на карте'*/
+  mapPins.addEventListener('click', showPopUps);
+
+  // удаляет обработчики с элемента 'Главный пин'
+  mapPinMain.removeEventListener('mouseup', activatePage);
+  mapPinMain.removeEventListener('keydown', onMainPinPressEnter);
+};
+
+// изменяет доступность для редактирования
+var changeAccessibility = function (array) {
+  array.forEach(function (elem) {
+    elem.disabled = (elem.disabled) ? false : true;
+  });
+};
+
+// активирует страницу при нажатии клавиши Enter при фокусе на главном пине
+var onMainPinPressEnter = function (evt) {
+  if (evt.key === 'Enter') {
+    activatePage();
+  }
+};
+
+var showPopUps = function (evt) {
+  var target = evt.target.closest('.map__pin');
+
+  if (target && target !== mapPinMain) {
+    if (lastActiveElement) {
+      lastActiveElement.classList.remove('.map__pin--active');
+      popup.remove();
+    }
+    target.classList.add('.map__pin--active');
+    lastActiveElement = target;
+    popup = renderCard(ads[lastActiveElement.number]);
+    map.appendChild(popup);
+
+    popupClose = map.querySelector('.popup__close');
+    popupClose.addEventListener('click', closePopup);
+    document.addEventListener('keydown', onPopupEscPress);
+
+  }
+  if (popup && target === mapPinMain) {
+    closePopup();
+  }
+};
+
+var closePopup = function () {
+  lastActiveElement.classList.remove('.map__pin--active');
+  popup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+var onPopupEscPress = function (evt) {
+  console.log(evt.key);
+  if (evt.key === 'Escape') {
+    closePopup();
+  }
+};
 /* <=== /FUNCTIONS ===> */
 
 
@@ -270,24 +337,23 @@ var createAds = function () {
 
 // создает все объявления
 var ads = createAds();
+// Формирует из DOM-коллекции массив
+fieldsets = Array.prototype.slice.call(fieldsets);
 
-// рендерит все пины
-var fragment = document.createDocumentFragment();
-for (var j = 0; j < ads.length; j++) {
-  fragment.appendChild(renderPin(ads[j]));
-}
-// добавляет созданные пины в DOM
-mapPins.appendChild(fragment);
+// Делает все поля формы недоступными в момент открытия страницы
+changeAccessibility(fieldsets);
+
+// Добавляет обработчики на элемент 'Главный пин'
+mapPinMain.addEventListener('mouseup', activatePage);
+mapPinMain.addEventListener('keydown', onMainPinPressEnter);
 
 // создает новый фрагмент
-fragment = document.createDocumentFragment();
-
+// fragment = document.createDocumentFragment();
 // рендерит одну карточку
-fragment.appendChild(renderCard(ads[0]));
+// fragment.appendChild(renderCard(ads[0]));
+//
+// добавляет карточку объявления в DOM
+// map.appendChild(fragment);
 
-// добавляет карточку обяъвления в DOM
-map.appendChild(fragment);
-
-map.classList.remove('map--faded');
 
 /* <=== /FUNCTION CALLS ===> */
