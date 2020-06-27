2b575a67ed3c90ad85fa5f707baf1a39ed464c80
dvasilchuk <dvasilchuk@phoenixit.ru>
2020-06-27 22:10:52 +0300

    Добавит функции send и load для работы с сервером

--- js/actions.js
+++ js/actions.js
@@ -1,6 +1,56 @@
 'use strict';
 
-(function () {
-  URL = 'https://javascript.pages.academy/keksobooking/';
+window.backend = (function () {
+  var StatusCode = {
+    200: 'ОК',
+    400: 'Неверный запрос',
+    401: 'Пользователь не авторизован',
+    404: 'Ничего на найдено',
+    500: 'Ошибка сервера'
+  };
+  var TIMEOUT_IN_MS = 10000;
+  var URL = 'https://javascript.pages.academy/keksobooking/';
 
-}) ();
+  var request = function (req, error) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        req(xhr.response);
+      } else {
+        error(StatusCode[xhr.status]);
+      }
+    });
+
+    xhr.addEventListener('error', function() {
+      error('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function() {
+      error('Привышен интервал ожидания запроса');
+    });
+
+    xhr.timeout = 10000;
+    return xhr;
+  };
+
+
+  var load = function (req, error) {
+    var xhr = request(req, error);
+
+    xhr.open('GET', URL + 'data');
+    xhr.send();
+  };
+
+  var send = function(data, req, error) {
+    var xhr = request(req, error);
+    xhr.open('POST', URL);
+    xhr.send(data);
+  };
+
+  return {
+    load: load,
+    send: send
+  };
+})();
